3dcbcf5bee6a63290062edc5b051068e56abe41d
nozomuuu <nzm1617@gmail.com>
2024-10-21 00:58:42 +0900

    Updated Vite configuration and project structure

--- new-otakarakuta-collection/vite.config.js
+++ new-otakarakuta-collection/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 
-export default {
-  root: 'new-otakarakuta-collection',
+export default defineConfig({
+  root: path.resolve(__dirname, 'new-otakarakuta-collection'),
   build: {
+    outDir: path.resolve(__dirname, 'build'),
     rollupOptions: {
-      input: 'index.html'
+      input: path.resolve(__dirname, 'new-otakarakuta-collection', 'index.html')
     }
   }
-};
-
+});
